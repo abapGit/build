51b452d5d4f13e74fec5793060999797c14a56f5
Alexander Tsybulsky <sbcgua@users.noreply.github.com>
2022-11-18 10:46:24 +0200

    attempt 2

--- ci/push-tag.sh
+++ ci/push-tag.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 # Used indirectly by abapGit ci, clone of abapgit/ci/push-tag.sh
 TAG="$1"
-echo $TAG
 
 if [ -z $TAG ]; then
     echo "Failed: Tag was not specified"
     exit 1
 fi
+echo "Tag to set: $TAG"
+
 if [ -z $GIT_USER_EMAIL ] || [ -z $GIT_USER_NAME ]; then
     echo "Failed: Git user name and email must be defined via env"
     exit 1
@@ -14,11 +15,9 @@ fi
 git config user.email "$GIT_USER_EMAIL"
 git config user.name "$GIT_USER_NAME"
 
-git remote -v
-git remote -v | grep -m1 '^origin'
-git remote -v | grep -m1 '^origin' | sed -Ene 's#.*(https://[^[:space:]]+).*#\1#p'
-
 REPO_URL=$(git remote -v | grep -m1 '^origin' | sed -Ene 's#.*(https://[^[:space:]]+).*#\1#p')
+echo "Repo URL: $REPO_URL"
+
 PUSH_URL=$(echo "$REPO_URL" | sed -Ene "s#(https://)#\1$GITHUB_API_KEY@#p")
 # e.g. https://$GITHUB_API_KEY@github.com/larshp/abapGit.git
 
