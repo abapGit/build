557c83c769b2d2e244fc4a2b35976e7d3502750c
Alexander Tsybulsky <sbcgua@users.noreply.github.com>
2022-11-18 10:39:05 +0200

    test

--- ci/push-tag.sh
+++ ci/push-tag.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Used indirectly by abapGit ci, clone of abapgit/ci/push-tag.sh
 TAG="$1"
+echo $TAG
 
 if [ -z $TAG ]; then
     echo "Failed: Tag was not specified"
@@ -13,9 +14,13 @@ fi
 git config user.email "$GIT_USER_EMAIL"
 git config user.name "$GIT_USER_NAME"
 
+git remote -v
+git remote -v | grep -m1 '^origin'
+git remote -v | grep -m1 '^origin' | sed -Ene 's#.*(https://[^[:space:]]+).*#\1#p'
+
 REPO_URL=$(git remote -v | grep -m1 '^origin' | sed -Ene 's#.*(https://[^[:space:]]+).*#\1#p')
 PUSH_URL=$(echo "$REPO_URL" | sed -Ene "s#(https://)#\1$GITHUB_API_KEY@#p")
 # e.g. https://$GITHUB_API_KEY@github.com/larshp/abapGit.git
 
-git tag $TAG || exit 1
-git push $PUSH_URL $TAG || exit 1
+# git tag $TAG || exit 1
+# git push $PUSH_URL $TAG || exit 1
